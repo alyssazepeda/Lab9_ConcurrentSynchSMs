203c6c3e09ed7edf970d0935c6911d75b37c1238
Alyssa Zepeda <azepe015@ucr.edu>
2021-07-14 22:29:37 -0700

    complete part 1

--- header/timer.h
+++ header/timer.h
@@ -1,43 +1,55 @@
-/////////////////////////////////////////////////////////////
-//TimerISR()
-void TimerISR();/* {
-       unsigned char i;
-        for (i = 0; i < tasksSize; ++i) {
-                if ( tasks[i].elapsedTime >= tasks[i].period ) {
-                        tasks[i].state = tasks[i].TickFct(tasks[i].state);
-                        tasks[i].elapsedTime = 0;
-                }
-                tasks[i].elapsedTime += tasksPeriod;
-        }
-}*/
-/////////////////////////////////////////////////////////////
-//timer
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
+// Permission to copy is granted provided that this header remains intact. 
+// This software is provided with no warranties.
 
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1ms
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+// Set TimerISR() to tick every M ms
 void TimerSet(unsigned long M) {
-    _avr_timer_M = M;
-    _avr_timer_cntcurr = _avr_timer_M;
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
 }
 
 void TimerOn() {
-    TCCR1B = 0x0B;
-    OCR1A = 125;
-    TIMSK1 = 0x02;
-    TCNT1 = 0;
-    _avr_timer_cntcurr = _avr_timer_M;
-    SREG |= 0x80;
+	// AVR timer/counter controller register TCCR1
+	TCCR1B 	= 0x0B;	// bit3 = 1: CTC mode (clear timer on compare)
+					// bit2bit1bit0=011: prescaler /64
+					// 00001011: 0x0B
+					// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+					// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A 	= 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+					// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+					// So when TCNT1 register equals 125,
+					// 1 ms has passed. Thus, we compare to 125.
+					// AVR timer interrupt mask register
+
+	TIMSK1 	= 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1 = 0;
+
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+	_avr_timer_cntcurr = _avr_timer_M;
+
+	//Enable global interrupts
+	SREG |= 0x80;	// 0x80: 1000000
 }
 
 void TimerOff() {
-    TCCR1B = 0x00;
+	TCCR1B 	= 0x00; 
 }
 
-/*ISR(TIMER1_COMPA_vect) {
-    _avr_timer_cntcurr--;
-    if (_avr_timer_cntcurr == 0){
-        TimerISR();
-        _avr_timer_cntcurr = _avr_timer_M;
-    }
-}*/
 
+#endif //TIMER_H
